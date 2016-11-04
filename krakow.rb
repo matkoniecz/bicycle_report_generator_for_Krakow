e06ec59079ca492fdb10ba0ea33718e6a5395ecf
Mateusz Konieczny <matkoniecz@gmail.com>
2016-11-04 20:33:28 +0100

    krakow-specific generator

---
+++ krakow.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+require_relative '../bicycle_report-gem/lib/bicycle_report.rb'
+
+lat_min = 50.00
+lat_max = 50.12
+lon_min = 19.78
+lon_max = 20.09
+main_generator = ReportGenerator.new(lat_min, lat_max, lon_min, lon_max)
+main_generator.set_language(:pl)
+main_generator.register_linkable_source("https://zikit.cartodb.com")
+main_generator.register_linkable_source("http://www.ankietarowerowa.pl/")
+main_generator.register_linkable_source("https://www.facebook.com/KrakowMiastemRowerow/")
+main_generator.register_linkable_source("http://ibikekrakow.com/")
+main_generator.register_linkable_source("https://twitter.com/oficerKRK")
+main_generator.register_linkable_source("https://www.facebook.com/ZIKiT.Krakow/")
+main_generator.register_linkable_source("http://www.skyscrapercity.com/showthread.php?t=642754")
+krakow_crossing = CrossingReportGenerator.new(lat_min, lat_max, lon_min, lon_max)
+krakow_bicycle_parking = BicycleParkingRaportGenerator.new(lat_min, lat_max, lon_min, lon_max)
+bicycle_ways = BicycleWayRaportGenerator.new(lat_min, lat_max, lon_min, lon_max)
+
+main_generator.start_writing_summary_pages()
+krakow_bicycle_parking.generate_statistics_about_bicycle_parkings
+bicycle_ways.process
+krakow_crossing.generate_html_files_about_crossings
+main_generator.copy_css()
+main_generator.finish_writing_summary_pages()
